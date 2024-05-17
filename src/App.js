f2a8dbfca96693e05b282da0155d2a6bf9991b9b
Eduardo Surface <dardoxy@gmail.com>
2024-05-17 12:41:19 -0600

    Test elipse take picture 2

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ const App = () => {
   const [picture, setPicture] = useState(null);
   const [showCamera, setShowCamera] = useState(false)
   return (
-    <div className='h-[100svh] flex justify-center items-center'>
+    <div className='h-[100svh] flex flex-col justify-center items-center'>
       { !showCamera ? 
-        <div className='p-4 flex flex-col gap-4 border'>
-          <div className=''>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 w-[120px]'>Subir archivo</button>
+        <div className='h-[100svh] flex flex-col justify-center items-center'>
+          <div>
+            {picture && (
+                <div>
+                <img src={picture} alt="Screenshot" />
+                </div>
+            )}
           </div>
-          <div className=''>
-            <button onClick={()=>setShowCamera(!showCamera)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 w-[120px]'>Tomar foto</button>
+          <div className='p-4 flex flex-col gap-4 border'>
+            <div className=''>
+              <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 w-[120px]'>Subir archivo</button>
+            </div>
+            <div className=''>
+              <button onClick={()=>setShowCamera(!showCamera)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2 w-[120px]'>Tomar foto</button>
+            </div>
           </div>
         </div>
         :
-        <WebCamCaptureController /> 
+        <WebCamCaptureController setShowCamera={setShowCamera} setPicture={setPicture}/> 
       }
     </div>
   );
--- src/components/WebCamCaptureController.js
+++ src/components/WebCamCaptureController.js
@@ -1,13 +1,14 @@
 import React, { useRef, useState, useCallback } from 'react';
 import Webcam from 'react-webcam';
 
-const WebCamCaptureController = () => {
+const WebCamCaptureController = ({setShowCamera, setPicture}) => {
     const webcamRef = useRef(null);
     const [url, setUrl] = useState(null);
 
-    const capturePhoto = useCallback(async () => {
+    const capturePicture = useCallback(async () => {
         const imageSrc = webcamRef.current.getScreenshot();
-        setUrl(imageSrc);
+        setPicture(imageSrc);
+        setShowCamera(false)
     }, [webcamRef]);
 
     const videoConstraints = {
@@ -15,26 +16,34 @@ const WebCamCaptureController = () => {
         facingMode: { exact: "user" }
     };
     return (
-        <div className='rounded-full relative h-[480px] w-full sm:w-[200px] sm:h-[200px] overflow-hidden border'>
-            <div className='absolute top-4'>
-                <Webcam
-                    ref={webcamRef}
-                    audio={false}
-                    height={500}
-                    width={400}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={videoConstraints}
-                >
-                </Webcam>
-            </div>
-            <button onClick={capturePhoto}>Capture</button>
-            <button onClick={() => setUrl(null)}>Refresh</button>
-            {url && (
-                <div>
-                <img src={url} alt="Screenshot" />
+        <div className='flex flex-col gap-4 items-center'>
+            <div className='rounded-[50%] h-[300px] w-[200px] sm:h-[200px] overflow-hidden border'>
+                <div className='relative w-[400px]'>
+                    <div className='absolute -left-[52%]'>
+                        <Webcam
+                            ref={webcamRef}
+                            audio={false}
+                            height={450}
+                            width={600}
+                            screenshotFormat="image/jpeg"
+                            videoConstraints={videoConstraints}
+                        >
+                        </Webcam>
+                    </div>
                 </div>
-            )}
+                
+            </div>
+            <div>
+                <button className='rounded-full border h-[40px] w-[40px]' onClick={capturePicture}></button>
+                {/*<button onClick={() => setUrl(null)}>Refresh</button>
+                {url && (
+                    <div>
+                    <img src={url} alt="Screenshot" />
+                    </div>
+                )}*/}
+            </div>
         </div>
+        
     );
 };
 
